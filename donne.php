b5edc19ebd933e8e402b18e3cab2fdf7ce018287
William Tchang <william.tchang@etu.u-bordeaux.fr>
2024-01-22 21:51:54 +0100

    html to php

---
+++ donne.php
@@ -0,0 +1,55 @@
+<?php
+$images = [
+    "meteo_mokaia_logo.png",
+    "mkzik.png",
+    "ecoGraph.png",
+    "labyrinthe.png",
+    "logo_labeyrie.png",
+    "pong.png"
+];
+
+$title = [
+    "Météo Mokaia (2023)",
+    "Mkzik (2023)",
+    "EchoGraph (2023)",
+    "Labyrinthe (2023)",
+    "Labeyrie (2021)",
+    "Pong (2021)"
+];
+$description = [
+    "Site web dédiée à la visualisation de la météo d'une Français à l'aide de son nom ou
+    encore
+    son code postale.",
+
+    "Mkzik un site web de musique streaming avec sessions utilisateurs. Vous avez la
+    possibilité
+    de poster vos musique en toute liberté sans aucune restriction et gratuitements.",
+
+    "Une application permettant de visualisé des données d'emmisions de gaz à effets de serres
+    en Europes selon les secteurs d'activitées.",
+
+    "Un jeu de Labyrinthe avec plusieurs labyrinthe et monstres rodant dans les couloires.
+    Objectifs sortir sans se faire tuée.",
+
+    "Labeyrie un site web static de présentaion de la franchise Labeyrie.",
+
+    "Jeu est inspiré du tennis de table en vue de dessus, et chaque joueur s'affronte en déplaçant la raquette virtuelle de haut en bas,
+     via les fléches haut et bas du clavier"
+];
+$links = [
+    "https://meteo.mokaia.fr",
+    "https://mkzik.fr/user/profile.php?id=50",
+    "",
+    "",
+    "https://zewills987.github.io/labeyrie/"
+];
+$retext = [];
+
+$skills = [
+    ["JavaScript", "API", "HTML", "CSS"],
+    ["PHP", "JavaScript", "HTML", "CSS", "MySQL"],
+    ["C#", "Python",],
+    ["Java", "JavaFx"],
+    ["HTML", "CSS"],
+    ["Java (processing)"]
+];
