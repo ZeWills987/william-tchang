0c20d66ef21d1c911259c209a7759b69f57d1aa0
William <william.tchang.pro@gmail.com>
2026-02-28 01:36:05 +0000

    prod

--- public/index.php
+++ public/index.php
@@ -1,9 +1,6 @@
 <?php
 
 use App\Kernel;
-use Symfony\Component\ErrorHandler\Debug;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Bundle\DebugBundle;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
